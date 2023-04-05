901ebe07648516356525312833e2843d7453088e
Fabian Gelin <fabian.gelin9@etu.univ-lorraine.fr>
2023-04-05 14:15:08 +0200

    ⌛ - Ajout des différentes commandes pour que le code compile (Telecommande.java)

---
+++ src/Telecommande.java
@@ -0,0 +1,14 @@
+public class Telecommande {
+
+    public void activerLampe(int i) {
+        throw new Error("code non écrit");
+    }
+
+    public void ajouterLampe(Lampe l) {
+        throw new Error("code non écrit");
+    }
+
+    public void desactiverLampe(int choix) {
+        throw new Error("code non écrit");
+    }
+}
