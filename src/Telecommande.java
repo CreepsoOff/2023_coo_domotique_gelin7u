847fd31eff018ef437c55940963df5b62ae3682d
Fabian Gelin <fabian.gelin9@etu.univ-lorraine.fr>
2023-04-05 15:03:19 +0200

    ➕ - Ajout de méthodes pour gérer Hifi
    Des méthodes dans Telecommande ont été ajoutées afin de pouvoir gérer les Hifi. Des getters ont également été rajoutés à des fins de test.
    
    Nous pouvons nous rendre compte que ce n'est pas pratique de copier/coller du code à chaque fois, une classe abstraite serait plus adaptée pour cette situation gérant les différents périphériques.

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Telecommande {
     private ArrayList<Lampe> lampes;
+    private ArrayList<Hifi> hifis;
 
     public Telecommande() {
-        this.lampes = new ArrayList<Lampe>();
+        this.lampes = new ArrayList<>();
+        this.hifis = new ArrayList<>();
 
     }
 
@@ -12,32 +15,65 @@ public class Telecommande {
         this.lampes.add(l);
     }
 
+    public void ajouterHifi(Hifi h) {
+        this.hifis.add(h);
+    }
+
     public void activerLampe(int i) {
         this.lampes.get(i).allumer();
+    }
+
+    public void activerHifi(int i) {
+        this.hifis.get(i).allumer();
     }
 
     public void desactiverLampe(int choix) {
         this.lampes.get(choix).eteindre();
     }
 
+    public void desactiverHifi(int choix) {
+        this.hifis.get(choix).eteindre();
+    }
+
     public void activerTout() {
         for (Lampe l : this.lampes) {
             l.allumer();
         }
+
+        for (Hifi h : this.hifis) {
+            h.allumer();
+        }
     }
 
+
+
     public String toString() {
-        String res = "";
-        for (Lampe l : this.lampes) {
-            res += l.toString() + " ";
+        StringBuilder res = new StringBuilder();
+        for (Lampe lampe : lampes) {
+            res.append(lampe.toString()).append("\n");
         }
-        return res;
+        for (Hifi hifi : hifis) {
+            res.append(hifi.toString()).append("\n");
+        }
+        return res.toString();
     }
 
     // Getters and Setters
 
     public Lampe getLampe(int i) {
         return this.lampes.get(i);
+    }
+
+    public Hifi getHifi(int i) {
+        return this.hifis.get(i);
+    }
+
+    public List<Lampe> getLampes() {
+        return this.lampes;
+    }
+
+    public List<Hifi> getHifis() {
+        return this.hifis;
     }
 
 
